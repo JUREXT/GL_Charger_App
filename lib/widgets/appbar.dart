41d027b6ce1e419960d2782c200a425825d83e17
JureLokovsek <lokovsek.jure@gmail.com>
2021-05-13 23:10:02 +0200

    Created basic app toolbar.

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -6,10 +6,7 @@ Widget appBar1(String title) {
   return AppBar(
     title: Text(title, style: TextStyle(fontSize: 24.0, color: Constants.lightBG)),
     bottom: PreferredSize(
-        child: Container(
-          color: Colors.orange,
-          height: 2.0,
-        ),
+        child: Container(color: Colors.orange, height: 2.0),
         preferredSize: Size.fromHeight(2.0)),
   );
 }
