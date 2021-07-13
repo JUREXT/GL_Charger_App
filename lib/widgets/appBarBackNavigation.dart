c4be061bfee7b6ca523837152446516a5d5a4113
JureLokovsek <lokovsek.jure@gmail.com>
2021-07-13 20:07:15 +0200

    Deleted no more needed widgets.

--- lib/widgets/appBarBackNavigation.dart
+++ lib/widgets/appBarBackNavigation.dart
@@ -5,7 +5,7 @@ import '../utils/constants.dart';
 Widget appBarBackNavigation(BuildContext context) {
   return AppBar(
     backgroundColor: Constants.ColorLightGrey,
-    elevation: 0, // remove shadow under toolbar
+    elevation: 0, // removes shadow under toolbar
     automaticallyImplyLeading: true,
       leading: IconButton(icon:Icon(Icons.arrow_back),
         onPressed:() => Navigator.pop(context, false),
