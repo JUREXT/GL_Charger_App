fd012bfe0f2acffec7d99c64f9c0583630a92da5
JureLokovsek <lokovsek.jure@gmail.com>
2021-07-20 22:54:57 +0200

    Created circular widget.

---
+++ lib/stateless_widget_components/circular_loader.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:gl_charge_app/stateless_widget_components/text_custom.dart';
+import 'package:gl_charge_app/utils/constants.dart';
+
+class ButtonYellow extends StatelessWidget {
+
+  final GestureTapCallback onPressed;
+  final String text;
+
+  ButtonYellow({@required this.onPressed, @required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16),
+      width: double.infinity,
+      height: 80,
+      child: RaisedButton(
+        child: TextCustom(text: text, textSize: 17.0, textColor: Constants.ColorBlack, decoration: TextDecoration.none),
+        color: Constants.ColorYellow,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
