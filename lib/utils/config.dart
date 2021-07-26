b2f52607e405c5f2e389f2a6782b5fcc32e458c0
JureLokovsek <lokovsek.jure@gmail.com>
2021-07-26 22:43:59 +0200

    Updated gerx and added storage lin an created storage util.

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,7 +1,13 @@
 import 'package:fimber/fimber.dart';
+import 'package:get_storage/get_storage.dart';
 
 class Config {
-  static init() async  {
+  static init() {
     Fimber.plantTree(DebugTree(useColors: true));
+    initGetStorage();
+  }
+
+  static initGetStorage() async {
+    await GetStorage.init();
   }
 }
